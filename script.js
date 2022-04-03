f5f4549b70cddbb01885db94acb957f14f2130f0
Zi J Wang <71153277+ZiWang55@users.noreply.github.com>
2022-04-02 22:21:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 const options = {
-	method: 'GET',
-	headers: {
-		accept: 'application/json',
-		'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com',
-		'X-RapidAPI-Key': '9e3e0e4eebmsh0c66b6e438da666p10acd5jsn572fc3847907'
-	}
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com',
+        'X-RapidAPI-Key': '9e3e0e4eebmsh0c66b6e438da666p10acd5jsn572fc3847907'
+    }
 };
-chuckJokes()	.catch(err => console.error(err));
+chuckJokes().catch(err => console.error(err));
 ;
 async function chuckJokes() {
     const response = await fetch('https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random', options);
-    const chuck = await response.json()
+    const chuck = await response.json()//put it into json
     console.log(chuck)
-    const value = await chuck.value
+    const value = await chuck.value//grabbing the jokes
     console.log(value)
-    container.textContent=value
+    const icon = await chuck.icon_url//grabbing the icon
+    //DOM
+    container.textContent = value
+    icondiv.src=icon
 }
+
+//old promises code
+
 // fetch('https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random', options)
 // 	.then(response =>  response.json())
 //     .then(response => {
@@ -26,6 +32,8 @@ async function chuckJokes() {
 // 	.catch(err => console.error(err));
 
 const app = document.getElementById('root');
-const icon = document.createElement('img')
+const icondiv = document.createElement('img')
 const container = document.createElement('div')
+
+app.appendChild(icondiv)
 app.appendChild(container)
